4518fdbfe407592fd354ea479a1f007779363eee
Jaap <jhpa.rodenburg@gmail.com>
2019-04-10 17:19:14 +0200

    Card delay

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -1,65 +1,89 @@
 console.log("Get Maps.js loaded!");
 var i;
+var jsonstring;
+var jsonstringparse;
 
 $.getJSON( "https://gewoonjaap.github.io/FortniteCreativeMaps/assets/json/maps.json", function( json ) {
+    jsonstring = json;
     console.log(json.maps.length)
 for (i = 0, l = json.maps.length; i < l; i++) {
+    AddCreativeMapDelay(i)
 
     console.log(json.maps[i].SAC);
     console.log(json.maps[i].Author);
     console.log(json.maps[i].IMGUrl);
     console.log(json.maps[i].MapCode);
-    //Create card div
+
+
+
+
+
+}
+});
+
+
+function AddCreativeMapDelay(url){
+    
+    jsonstringparse = jsonstring;
+    console.log(jsonstringparse.maps[url].Author + " lol2")
+    var min = 1,
+       max = 2;
+       id = url;
+   var rand = Math.floor(Math.random() * (max - min + 1) + min);
+   setTimeout(function(){ AddCreativeMap(url); }, rand * 1000);
+   
+   }
+
+   //Add map to site
+
+   function AddCreativeMap(arrayid){
+       //Create CARD
+
     var mapdiv = document.createElement("div");             
     mapdiv.setAttribute('class', 'mapcard' );
-    mapdiv.setAttribute('id', 'mapcard' + json.maps[i].MapCode );
+    mapdiv.setAttribute('id', 'mapcard' + jsonstring.maps[arrayid].MapCode );
     document.getElementById('cards').appendChild(mapdiv);
-   
 
-    // Add Author
-
-
+// Add Card Title
+       console.log(jsonstring.maps[arrayid].Author)
     var CardTitleP = document.createElement("P");   
-    CardTitleP.innerHTML = json.maps[i].Author;          
+    CardTitleP.innerHTML = jsonstring.maps[arrayid].Author;          
     CardTitleP.setAttribute('class', 'cardtitle' );
-    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardTitleP);   
+    document.getElementById('mapcard' + jsonstring.maps[arrayid].MapCode).appendChild(CardTitleP);   
    
    
     //Add image
 
     var CardImage = document.createElement("P");   
-    CardImage.innerHTML = "<a href='" +json.maps[i].IMGUrl + "' data-lightbox='" + json.maps[i].MapCode + "'><img src='" +json.maps[i].IMGUrl + "' class='cardimg'></img></a>"         
+    CardImage.innerHTML = "<a href='" +jsonstringparse.maps[arrayid].IMGUrl + "' data-lightbox='" + jsonstringparse.maps[arrayid].MapCode + "'><img src='" +jsonstringparse.maps[arrayid].IMGUrl + "' class='cardimg'></img></a>"         
     CardImage.setAttribute('class', 'cardimagecode' );
-    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardImage);   
+    document.getElementById('mapcard' + jsonstringparse.maps[arrayid].MapCode).appendChild(CardImage);   
 
 
     //Add Map Code
     var CardMapCode = document.createElement("P");   
-    CardMapCode.innerHTML = "<a href='https://fortnite.com/fn/" + json.maps[i].MapCode + "'>Map Code: " + json.maps[i].MapCode + "</a>";          
+    CardMapCode.innerHTML = "<a href='https://fortnite.com/fn/" + jsonstringparse.maps[arrayid].MapCode + "'>Map Code: " + jsonstringparse.maps[arrayid].MapCode + "</a>";          
     CardMapCode.setAttribute('class', 'cardmapcode' );
-    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardMapCode);   
+    document.getElementById('mapcard' + jsonstringparse.maps[arrayid].MapCode).appendChild(CardMapCode);   
 
 
 
         //Add SAC
         var CardMapCode = document.createElement("P");   
-        CardMapCode.innerHTML = "Support-a-Creator: " + json.maps[i].SAC;          
+        CardMapCode.innerHTML = "Support-a-Creator: " + jsonstringparse.maps[arrayid].SAC;          
         CardMapCode.setAttribute('class', 'cardmapcode' );
-        document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardMapCode);   
+        document.getElementById('mapcard' + jsonstringparse.maps[arrayid].MapCode).appendChild(CardMapCode);   
 
 
 
-for (i2 = 0, l2 = json.maps[i].IMG2.length; i2 < l2; i2++) {
-    console.log(json.maps[i].IMG2[i2]);
+for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
+    console.log(jsonstringparse.maps[arrayid].IMG2[i2]);
 
         //Add image
 
         var CardImage = document.createElement("P");   
-        CardImage.innerHTML = "<a href='" +json.maps[i].IMG2[i2] + "' data-lightbox='" + json.maps[i].MapCode + "'></a>"         
+        CardImage.innerHTML = "<a href='" +jsonstringparse.maps[arrayid].IMG2[i2] + "' data-lightbox='" + jsonstringparse.maps[arrayid].MapCode + "'></a>"         
         CardImage.setAttribute('class', 'cardimagecode' );
-        document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardImage);   
+        document.getElementById('mapcard' + jsonstringparse.maps[arrayid].MapCode).appendChild(CardImage);   
     }
-
-
-}
-});
+   }
