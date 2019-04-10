c8ae4c69e10d155acf204fe3c7951ce176b4e9d9
Jaap <jhpa.rodenburg@gmail.com>
2019-04-10 16:52:35 +0200

    Title and image

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -14,17 +14,24 @@ for (i = 0, l = json.maps.length; i < l; i++) {
     mapdiv.setAttribute('class', 'mapcard' );
     mapdiv.setAttribute('id', 'mapcard' + json.maps[i].MapCode );
     document.getElementById('cards').appendChild(mapdiv);
-    //Card Title
+   
+
+    // Add Author
+
+
+    var CardTitleP = document.createElement("P");   
+    CardTitleP.innerHTML = json.maps[i].Author;          
+    CardTitleP.setAttribute('class', 'cardtitle' );
+    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardTitleP);   
+   
+   
+    //Add image
 
     var CardImage = document.createElement("P");   
     CardImage.innerHTML = "<a href='" +json.maps[i].IMGUrl + "' data-lightbox='" + json.maps[i].MapCode + "'><img src='" +json.maps[i].IMGUrl + "' class='cardimg'></img></a>"         
     CardImage.setAttribute('class', 'cardimagecode' );
     document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardImage);   
-//Add Image
-    var CardTitleP = document.createElement("P");   
-    CardTitleP.innerHTML = json.maps[i].Author;          
-    CardTitleP.setAttribute('class', 'cardtitle' );
-    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardTitleP);   
+
 
     //Add Map Code
     var CardMapCode = document.createElement("P");   
