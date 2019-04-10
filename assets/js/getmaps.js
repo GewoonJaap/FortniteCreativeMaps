d30682b506c328078c06bfc783403b9c9a8c67cc
Jaap <jhpa.rodenburg@gmail.com>
2019-04-10 16:07:54 +0200

    404

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -1,5 +1,4 @@
 console.log("Get Maps.js loaded!");
 
-$.getJSON( "./assets/json/maps.json", function( json ) {
-    console.log( "JSON Data received, name is " + json);
-});
+var ParsedJSON = JSON.parse('https://gewoonjaap.github.io/FortniteCreativeMaps/assets/json/maps.json');
+console.log(ParsedJSON);
