b6f76935b08d2b850a6767e2e80a33a8458da6f8
Jaap <jhpa.rodenburg@gmail.com>
2019-04-10 16:37:54 +0200

    Iame url

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -9,14 +9,21 @@ for (i = 0, l = json.maps.length; i < l; i++) {
     console.log(json.maps[i].Author);
     console.log(json.maps[i].IMGUrl);
     console.log(json.maps[i].MapCode);
-    //Create div
+    //Create card div
     var mapdiv = document.createElement("div");             
     mapdiv.setAttribute('class', 'mapcard' );
-    mapdiv.setAttribute('id', 'mapcard' + json.maps[i].Author );
+    mapdiv.setAttribute('id', 'mapcard' + json.maps[i].MapCode );
     document.getElementById('cards').appendChild(mapdiv);
+    //Card Title
+
+    var CardImage = document.createElement("P");   
+    CardImage.innerHTML = "<a href='" +json.maps[i].IMGUrl + "' data-lightbox='" + json.maps[i].MapCode + "'><img src='" +json.maps[i].IMGUrl + "' class='cardimg'></img></a>"         
+    CardImage.setAttribute('class', 'cardimagecode' );
+    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardImage);   
+//Add Image
     var CardTitleP = document.createElement("P");   
     CardTitleP.innerHTML = json.maps[i].Author;          
     CardTitleP.setAttribute('class', 'cardtitle' );
-    document.getElementById('mapcard' + json.maps[i].Author).appendChild(CardTitleP);   
+    document.getElementById('mapcard' + json.maps[i].MapCode).appendChild(CardTitleP);   
 }
 });
