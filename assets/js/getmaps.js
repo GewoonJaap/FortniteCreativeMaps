ab900afb21a9457cec33491290e66f8f6f5379ae
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 17:08:25 +0200

    Update getmaps.js

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -114,11 +114,11 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
 
    document.addEventListener("DOMContentLoaded", function() {
    var listElm = document.querySelector("body");
+});
 
-   listElm.addEventListener('scroll', function() {
+listElm.addEventListener('scroll', function() {
     if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
         AutomaticAdder();
         console.log('scrolled');
     }
   });
-});
