49d60322256f949530464852a1c4597ab8cc33c1
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 19:53:41 +0200

    Music toggle

--- assets/js/core.js
+++ assets/js/core.js
@@ -12,7 +12,9 @@ function ToggleMusic()
 
 myAudio.onplaying = function() {
     isPlaying = true;
+    document.getElementById('icon').innerHTML = '<i class="material-icons">volume_off</i>';
   };
   myAudio.onpause = function() {
     isPlaying = false;
+    document.getElementById('icon').innerHTML = '<i class="material-icons">music_note</i>';
   };
