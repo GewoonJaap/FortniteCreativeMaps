7600b6871e5398b2656257ab32e04fe0588fb272
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 17:10:55 +0200

    Update getmaps.js

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -113,7 +113,7 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
 
 
    document.addEventListener("DOMContentLoaded", function() {
-   var listElm = document.querySelector("body");
+   var listElm = document.querySelectorAll("body");
    test();
 });
 function test(){
