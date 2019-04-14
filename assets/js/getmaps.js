f85a0797c2b6f5276306f6b77387fa42605698a2
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 19:07:54 +0200

    More maps

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -116,7 +116,7 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
 
    function AutomaticAdder(){
        document.getElementById('loader').style.visibility = 'visible';
-       MapsToGet = MapsToGet +3;
+       MapsToGet = MapsToGet +6;
        ForEachLoopAddNew(jsonstring);
        loopchecker();
    }
