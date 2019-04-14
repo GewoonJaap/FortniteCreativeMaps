9de8c2b7731c5c59d1636d1c554816bbf76a8f62
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 17:13:26 +0200

    Update getmaps.js

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -117,10 +117,11 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
    listElm = document.querySelectorAll("body");
    test();
 });
+test(){
 listElm.addEventListener('scroll', function() {
     if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
         AutomaticAdder();
         console.log('scrolled');
     }
   });
- 
+}
