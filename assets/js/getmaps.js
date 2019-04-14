32efb84f585dc3466a0dcf808d8b512ebd649e54
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 17:03:56 +0200

    scrol

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -112,9 +112,13 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
    }
 
 
+   document.addEventListener("DOMContentLoaded", function() {
+   var listElm = document.querySelector("body");
+
    listElm.addEventListener('scroll', function() {
     if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
         AutomaticAdder();
         console.log('scrolled');
     }
   });
+});
