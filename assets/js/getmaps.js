d1b79be4596de37ffc4120b0496a45d2b18374bc
Jaap <jhpa.rodenburg@gmail.com>
2019-04-14 16:54:19 +0200

    Scroller

--- assets/js/getmaps.js
+++ assets/js/getmaps.js
@@ -110,10 +110,3 @@ for (i2 = 0, l2 = jsonstringparse.maps[arrayid].IMG2.length; i2 < l2; i2++) {
        ForEachLoopAddNew(jsonstring);
    }
 
-
-   $('.container').infiniteScroll({
-    // options
-    path: 'AutomaticAdder',
-    append: '.cards',
-    history: false,
-  });
