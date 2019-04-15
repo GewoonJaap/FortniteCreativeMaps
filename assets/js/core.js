4548237cdc7a7cd2e460c34540a414ee34241c4b
Jaap <jhpa.rodenburg@gmail.com>
2019-04-15 18:29:20 +0200

    Logging

--- assets/js/core.js
+++ assets/js/core.js
@@ -12,10 +12,12 @@ function ToggleMusic()
 
 myAudio.onplaying = function() {
     isPlaying = true;
+    colorLog('[DJ Yonder] Playing music..', 'success');
     document.getElementById('icon').innerHTML = '<i class="material-icons">volume_off</i>';
   };
   myAudio.onpause = function() {
     isPlaying = false;
+    colorLog('[DJ Yonder] Pausing music..', 'success');
     document.getElementById('icon').innerHTML = '<i class="material-icons">music_note</i>';
   };
 
