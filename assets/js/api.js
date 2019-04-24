bce27b6d345b0b3a22c093bfab8fb9bc9e3abfc2
Jaap <jhpa.rodenburg@gmail.com>
2019-04-24 13:22:22 +0200

    Daily news

--- assets/js/api.js
+++ assets/js/api.js
@@ -11,7 +11,50 @@ $.ajaxSetup({
 
   $.getJSON( "https://rhetorical-clam.glitch.me/", function( json ) {
     jsonstring = json;
-    console.log(json);
+    console.log(jsonstring.battleroyalenews.news);
+    console.log(jsonstring.battleroyalenews.news.messages.length)
+    DailyNewsLoop(); 
 
 });
+
+
+function DailyNewsLoop(){
+    for (i = 0; i < jsonstring.battleroyalenews.news.messages.length; i++) { 
+        console.log(jsonstring.battleroyalenews.news.messages[i].image)
+        AddDailyNews(i);
+
+
+    }
+}
+
+function AddDailyNews(arrayid){
+//Create CARD
+var mapdiv = document.createElement("div");             
+mapdiv.setAttribute('class', 'dailynewscard' );
+mapdiv.setAttribute('id', 'dailynewscard' + arrayid );
+document.getElementById('dailynewsdiv').appendChild(mapdiv);
+
+// Add Card Title
+var CardTitleP = document.createElement("P");   
+CardTitleP.innerHTML = jsonstring.battleroyalenews.news.messages[arrayid].title;          
+CardTitleP.setAttribute('class', 'newstexthead' );
+document.getElementById('dailynewscard' + arrayid).appendChild(CardTitleP);   
+
+
+//Add image
+
+var CardImage = document.createElement("P");   
+CardImage.innerHTML = "<a href='" + jsonstring.battleroyalenews.news.messages[arrayid].image + "' data-lightbox='" + jsonstring.battleroyalenews.news.messages[arrayid].body + "'><img alt='Loading...' src='" +jsonstring.battleroyalenews.news.messages[arrayid].image + "' class='dailynewsimg'></img></a>"         
+//CardImage.setAttribute('class', 'dailynewsimg' );
+document.getElementById('dailynewscard' + arrayid).appendChild(CardImage);   
+
+
+//Add Map Code
+var CardMapCode = document.createElement("P");   
+CardMapCode.innerHTML = jsonstring.battleroyalenews.news.messages[arrayid].body;          
+CardMapCode.setAttribute('class', 'newstextnormal' );
+document.getElementById('dailynewscard' + arrayid).appendChild(CardMapCode);   
+ 
+
+}
 
