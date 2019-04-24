51877f1332b2569969aabb40ffc53fa5d498e635
Jaap <jhpa.rodenburg@gmail.com>
2019-04-24 11:19:10 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,6 +5,7 @@ GetData();
 
 function GetData(){
     $.getJSON( "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game?callback=?", function( data ){
+        console.log('hey');
         console.log(data); // Logs "jQuery Howto"
       });
 
