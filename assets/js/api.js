f8d6f8c073e5db43710da046f745cb7df2a30b87
Jaap <jhpa.rodenburg@gmail.com>
2019-04-24 12:45:58 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,12 +1,17 @@
 var jsonstring;
 var url = 'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game'
 
-GetData();
 
-function GetData(){
-    $.getJSON( "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game?callback=?", function( json ){
-        console.log('hey');
-        console.log(json.lastModified); // Logs "jQuery Howto"
-      });
-
+$.ajaxSetup({
+    headers : {
+      'Authorization' : 'Basic faskd52352rwfsdfs',
+      'Target-URL' : url
     }
+  });
+
+  $.getJSON( "https://rhetorical-clam.glitch.me/", function( json ) {
+    jsonstring = json;
+    console.log(json);
+
+});
+
