09f219b1280e2e07a916d036c7b3a817b8485336
Jaap <jhpa.rodenburg@gmail.com>
2019-04-24 11:18:14 +0200

    colo log

---
+++ assets/js/colorlog.js
@@ -0,0 +1,26 @@
+  // Console
+
+
+  function colorLog(message, color) {
+
+    color = color || "black";
+
+    switch (color) {
+        case "success":  
+             color = "Green"; 
+             break;
+        case "info":     
+                color = "DodgerBlue";  
+             break;
+        case "error":   
+             color = "Red";     
+             break;
+        case "warning":  
+             color = "Orange";   
+             break;
+        default: 
+             color = color;
+    }
+
+    console.log("%c" + message, "color:" + color);
+}
--- assets/js/core.js
+++ assets/js/core.js
@@ -22,30 +22,3 @@ myAudio.onplaying = function() {
   };
 
 
-
-  // Console
-
-
-  function colorLog(message, color) {
-
-    color = color || "black";
-
-    switch (color) {
-        case "success":  
-             color = "Green"; 
-             break;
-        case "info":     
-                color = "DodgerBlue";  
-             break;
-        case "error":   
-             color = "Red";     
-             break;
-        case "warning":  
-             color = "Orange";   
-             break;
-        default: 
-             color = color;
-    }
-
-    console.log("%c" + message, "color:" + color);
-}
