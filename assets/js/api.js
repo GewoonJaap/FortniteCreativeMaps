4cf9ddb1404542082f8b662722ae75ef639d4d51
Jaap <jhpa.rodenburg@gmail.com>
2019-04-24 11:11:06 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,31 +1,6 @@
 var jsonstring;
+var url = 'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game'
 
-
-(function() {
-    var api = "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game?format=json";
-    var request = $.getJSON( api, {
-        secret : 'secret', 
-        appId : 'app', 
-        emailId : 'abc@gmail.com',
-        async: false,
-        dataType : 'json',
-      },
-      function(data, result){
-        console.log(JSON.stringify(data));
+    $.getJSON( url+"?callback=?", function( data ){
+        console.log( data.title ); // Logs "jQuery Howto"
       });
-
-    request.complete(function(d, status){
-        console.log('Complete the request and got the data - ' + JSON.stringify(d) + '/' + status, filename);
-    });
-
-    request.error(function(err){
-        console.log('Error happened - ', filename);
-        console.log(err);
-    });
-
-    request.success(function( data, status, jqXHR ) {
-        console.log(data);
-    });
-
-
-    })();
