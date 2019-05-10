f21cda8062e33df961d51aa2aa1656a00c9e3e8e
Jaap <jhpa.rodenburg@gmail.com>
2019-05-10 14:29:08 +0200

    Playlist fix

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -14,16 +14,16 @@ $.ajaxSetup({
     jsonstring = json;
     colorLog('[Yarr] Setting up the background', 'success');
     document.getElementById('body').style.backgroundImage = 'url(' + jsonstring.subgameselectdata.battleRoyale.message.image +')';
-    document.getElementById('playlists').innerHTML = 'Total playlists available: ' + jsonstring.playlistimages.playlistimages.images.length;
-    document.getElementById('update').innerHTML = 'Updated on: ' + jsonstring.playlistimages.lastModified;
+    document.getElementById('playlists').innerHTML = 'Total playlists available: ' + jsonstring.playlistinformation.playlist_info.playlists.length;
+    document.getElementById('update').innerHTML = 'Updated on: ' + jsonstring.playlistinformation.lastModified;
     PlayListLoop();
 
 });
 
 
 function PlayListLoop(){
-    for (i = 0; i < jsonstring.playlistimages.playlistimages.images.length; i++) { 
-        console.log(jsonstring.playlistimages.playlistimages.images[i].image);
+    for (i = 0; i < jsonstring.playlistinformation.playlist_info.playlists.length; i++) { 
+        console.log(jsonstring.playlistinformation.playlist_info.playlists[i].image);
         AddPlayList(i);
 
 
@@ -42,7 +42,7 @@ document.getElementById('dailynewsbrdiv').appendChild(mapdiv);
 
 // Add Card Title
 var CardTitleP = document.createElement("P");   
-CardTitleP.innerHTML = jsonstring.playlistimages.playlistimages.images[arrayid].playlistname;          
+CardTitleP.innerHTML = jsonstring.playlistinformation.playlist_info.playlists[arrayid].playlist_name;          
 CardTitleP.setAttribute('class', 'newstexthead' );
 document.getElementById('dailynewscard' + arrayid).appendChild(CardTitleP);   
 
@@ -50,14 +50,14 @@ document.getElementById('dailynewscard' + arrayid).appendChild(CardTitleP);
 //Add image
 
 var CardImage = document.createElement("P");   
-CardImage.innerHTML = "<a href='" + jsonstring.playlistimages.playlistimages.images[arrayid].image + "' data-lightbox='" + jsonstring.playlistimages.playlistimages.images[arrayid].playlistname + "'><img alt='Loading...' src='" +jsonstring.playlistimages.playlistimages.images[arrayid].image + "' class='dailynewsimg'></img></a>"         
+CardImage.innerHTML = "<a href='" + jsonstring.playlistinformation.playlist_info.playlists[arrayid].image + "' data-lightbox='" + jsonstring.playlistinformation.playlist_info.playlists[arrayid].playlist_name + "'><img alt='Loading...' src='" +jsonstring.playlistinformation.playlist_info.playlists[arrayid].image + "' class='dailynewsimg'></img></a>"         
 CardImage.setAttribute('class', 'dailynewsimg2' );
 document.getElementById('dailynewscard' + arrayid).appendChild(CardImage);   
 
 
 //Add Map Code
 var CardMapCode = document.createElement("P");   
-CardMapCode.innerHTML = jsonstring.playlistimages.playlistimages.images[arrayid].playlistname;          
+CardMapCode.innerHTML = jsonstring.playlistinformation.playlist_info.playlists[arrayid].playlist_name;          
 CardMapCode.setAttribute('class', 'newstextnormal' );
 document.getElementById('dailynewscard' + arrayid).appendChild(CardMapCode);   
 
